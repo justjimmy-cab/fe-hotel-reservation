328d60656bf61eaf06638f5a4a8fca525c11dd2e
justjimmy-cab <jimmycabillan056@gmail.com>
2024-11-13 16:56:15 +0800

    Installed Materia Styles, created Homepage component

--- src/App.js
+++ src/App.js
@@ -3,18 +3,19 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-d
 import Login from './Login';
 import UserList from './UserList';
 import PrivateRoute from './PrivateRoute';
+import HomePage from './HomePage';
 
 function App() {
     return (
         <Router>
             <Routes>
+                <Route path="/" element={<HomePage />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/users" element={
                     <PrivateRoute>
                         <UserList />
                     </PrivateRoute>
                 } />
-                <Route path="*" element={<Navigate to="/login" />} /> {/* Redirect to login by default */}
             </Routes>
         </Router>
     );
